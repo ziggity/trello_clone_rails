92b46b238736a865df3ecf88ad94fa032e0295f4
zach beecher <zachbeecher@gmail.com>
2017-11-15 22:59:39 -0800

    styling

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,39 +1,37 @@
 class TasksController < ApplicationController
-    before_action :set_project
-    before_action :set_task, except: [:create]
+	before_action :set_project
+	before_action :set_task, except: [:create]
 
-    def create
-      @task = @project.tasks.create(task_params)
-      redirect_to @project 
-    end
+	def create
+		@task = @project.tasks.create(task_params)
+		redirect_to @project 
+	end
 
-    def destroy
-     
-        if @task.destroy
-            flash[:success] = "task was deleted."
-        else
-            flash[:error] = "task was not deleted"
-        end
-        redirect_to @project
-    end
+	def destroy
+		if @task.destroy
+			flash[:success] = "Task was deleted."
+		else
+			flash[:error] = "Task was not deleted."
+		end
+		redirect_to @project
+	end
 
-    def complete
-        @task.update_attribute(:completed_at, Time.now)
-        redirect_to @project, notice: "Task completed"
-    end
+	def complete
+		@task.update_attribute(:completed_at, Time.now)
+		redirect_to @project, notice: "Task Completed"
+	end
 
-    private 
+	private
 
-    def set_task
-        @task = @project.tasks.find(params[:id])
-    end
+	def set_project
+		@project = Project.find(params[:project_id])
+	end
 
-    def set_project
-      @project = Project.find(params[:project_id])
-    end
+	def set_task
+		@task = @project.tasks.find(params[:id])
+	end
 
-    def task_params
-        params[:task].permit(:content)
-    end
-
+	def task_params
+		params[:task].permit(:content)
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  
   resources :projects do
     resources :tasks do
-      member do
-        patch :complete
-      end
+    	member do 
+    		patch :complete
+    	end
     end
   end
+
+  root "projects#index"
 end
