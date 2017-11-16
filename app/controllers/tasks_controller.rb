2e38543a366aa323df64166c5a8c4370fe2a6d22
zach beecher <zachbeecher@gmail.com>
2017-11-15 22:27:52 -0800

    controller for tasks complete, destroy

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,20 +1,32 @@
 class TasksController < ApplicationController
     before_action :set_project
+    before_action :set_task, except: [:create]
 
     def create
-      @task = @project.task.create(task_params)
+      @task = @project.tasks.create(task_params)
       redirect_to @project 
     end
+
     def destroy
-        @task = @project.tasks.find(params[:id])
+     
         if @task.destroy
             flash[:success] = "task was deleted."
         else
             flash[:error] = "task was not deleted"
         end
+        redirect_to @project
+    end
+
+    def complete
+        @task.update_attribute(:completed_at, Time.now)
+        redirect_to @project, notice: "Task completed"
     end
 
     private 
+
+    def set_task
+        @task = @project.tasks.find(params[:id])
+    end
 
     def set_project
       @project = Project.find(params[:project_id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   resources :projects do
-    resources :tasks
+    resources :tasks do
+      member do
+        patch :complete
+      end
+    end
   end
 end
